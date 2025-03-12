e32931aa29d1473166655bc648a1ba7981d85b0e
Wilson Bilkovich <wilsonb@gmail.com>
2025-03-12 14:01:15 -0400

    feat: Configure tokyonight theme with detailed dark mode settings

--- init.lua
+++ init.lua
@@ -96,6 +96,23 @@ require("lazy").setup({
     lazy = false,
     priority = 1000,
     config = function()
+      require("tokyonight").setup({
+        style = "night",
+        light_style = "day",
+        transparent = false,
+        terminal_colors = true,
+        styles = {
+          comments = { italic = true },
+          keywords = { italic = true },
+          functions = {},
+          variables = {},
+        },
+        sidebars = { "qf", "help" },
+        day_brightness = 0.3,
+        hide_inactive_statusline = false,
+        dim_inactive = false,
+        lualine_bold = false,
+      })
       vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
