53318d5ad08a99df319557f1bce09719c57e07aa
Wilson Bilkovich <wilsonb@gmail.com>
2025-03-12 13:53:29 -0400

    refactor: Migrate from paq-nvim to lazy.nvim with enhanced Neovim configuration

--- init.lua
+++ init.lua
@@ -1,50 +1,157 @@
-local PKGS = {
-    { 'savq/paq-nvim' },
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
-    -- Tree-sitter
-    { 'nvim-treesitter/nvim-treesitter', build = function() cmd 'TSUpdate' end },
-    'nvim-treesitter/nvim-treesitter-textobjects',
+-- Set leader key before lazy setup
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-    -- LSP & Language plugins
-    'neovim/nvim-lspconfig',
-    'rust-lang/rust.vim',
+-- Plugin setup
+require("lazy").setup({
+  -- Tree-sitter
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = { "lua", "vim", "vimdoc", "rust" },
+        highlight = { enable = true },
+        indent = { enable = true },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+            },
+          },
+        },
+      })
+    end,
+  },
 
-    -- Copilot
-    'github/copilot.vim'
-  }
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+    config = function()
+      -- LSP setup will be defined later
+    end,
+  },
 
+  -- Rust
+  {
+    "rust-lang/rust.vim",
+    ft = "rust",
+    init = function()
+      vim.g.rustfmt_autosave = 1
+    end,
+  },
 
-local function clone_paq()
-  local path = vim.fn.stdpath 'data' .. '/site/pack/paqs/start/paq-nvim'
-  if vim.fn.empty(vim.fn.glob(path)) > 0 then
-    vim.fn.system {
-      'git',
-      'clone',
-      '--depth=1',
-      'https://github.com/savq/paq-nvim.git',
-      path,
-    }
-  end
+  -- Copilot
+  {
+    "github/copilot.vim",
+    event = "InsertEnter",
+  },
+})
+
+-- LSP Configuration
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- Setup common LSP servers
+local servers = { "lua_ls", "rust_analyzer" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    capabilities = capabilities,
+  })
 end
 
-clone_paq()
-vim.cmd 'packadd paq-nvim'
-package.loaded.plugins = nil
-require 'paq'(PKGS)
+-- LSP keymaps
+vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover documentation" })
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code action" })
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format, { desc = "Format" })
 
-do -- Copilot
-  vim.keymap.set('n', '<leader>p', function()
-    vim.cmd 'Copilot panel'
-  end)
-end
+-- Completion setup
+local cmp = require("cmp")
+local luasnip = require("luasnip")
 
-do -- Trailing whitespace
-  vim.fn.matchadd('WhitespaceTrailing', [[\s\{1,}$]])
-  vim.api.nvim_set_hl(0, 'WhitespaceTrailing', { link = 'diffText' })
-end
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-Space>"] = cmp.mapping.complete(),
+    ["<CR>"] = cmp.mapping.confirm({ select = true }),
+    ["<Tab>"] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { "i", "s" }),
+  }),
+  sources = cmp.config.sources({
+    { name = "nvim_lsp" },
+    { name = "luasnip" },
+    { name = "buffer" },
+    { name = "path" },
+  }),
+})
 
-do -- Tab config
-  vim.opt.tabstop = 2
-  vim.opt.shiftwidth = 2
-  vim.opt.expandtab = true
-end
+-- Copilot
+vim.keymap.set("n", "<leader>p", function()
+  vim.cmd("Copilot panel")
+end, { desc = "Open Copilot panel" })
+
+-- Trailing whitespace
+vim.fn.matchadd("WhitespaceTrailing", [[\s\{1,}$]])
+vim.api.nvim_set_hl(0, "WhitespaceTrailing", { link = "diffText" })
+
+-- Editor settings
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.signcolumn = "yes"
+vim.opt.termguicolors = true
+vim.opt.updatetime = 250
+vim.opt.timeoutlen = 300
+vim.opt.undofile = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Keymaps
+vim.keymap.set("n", "<leader>w", "<cmd>w<cr>", { desc = "Save" })
+vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Quit" })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<cr>", { desc = "Clear search highlight" })
