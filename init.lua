b152b0d0ae429c21a677b9c6fd6f2802f178f323
Wilson Bilkovich <wilsonb@gmail.com>
2025-03-12 13:54:42 -0400

    refactor: Remove relativenumber setting from init.lua

--- init.lua
+++ init.lua
@@ -142,7 +142,6 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.number = true
-vim.opt.relativenumber = true
 vim.opt.signcolumn = "yes"
 vim.opt.termguicolors = true
 vim.opt.updatetime = 250
