40b6b8a46778c872c8aadfa0832c2e6447c96dd6
Wilson Bilkovich <wilson@wb78.net>
2026-08-11 18:08:23 -0400

    Add <leader>p to paste at end of line with leading space. Add descriptions to LSP keymaps.

--- custom/keymaps.lua
+++ custom/keymaps.lua
@@ -4,20 +4,28 @@
 local M = {}
 
 function M.setup()
-  -- Global diagnostic keymaps
   vim.keymap.set('n', '<leader>w', vim.diagnostic.open_float, { noremap = true, silent = true, desc = "Show diagnostics float" })
   vim.keymap.set('n', '<leader>W', vim.diagnostic.setloclist, { noremap = true, silent = true, desc = "Show diagnostics list" })
+
+  vim.keymap.set('n', '<leader>p', function()
+    return 'A <esc>"' .. vim.v.register .. 'p'
+  end, { expr = true, desc = "Paste at end of line with leading space" })
 end
 
 -- Invoked on-attach by the LSP package.
 function M.setup_lsp(bufnr)
   local opts = { noremap = true, silent = true, buffer = bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
-  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, opts)
+  -- Add a description key to the map of options
+  local function add_desc(desc)
+    return vim.tbl_extend('force', opts, { desc = desc })
+  end
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, add_desc('Go to definition'))
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, add_desc('Hover documentation'))
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, add_desc('Rename symbol'))
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, add_desc('Code action'))
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, add_desc('Find references'))
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, add_desc('Format buffer'))
 end
 
 return M
